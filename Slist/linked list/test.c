449acf50cc98c5b46eea7797c8f6f7dab592e2c5
dawn <820583522@qq.com>
2021-06-21 19:21:30 +0800

    添加了双向带头链表的增删查改

--- Slist/linked list/test.c
+++ Slist/linked list/test.c
@@ -1,7 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include "SList.h"
-int main()
+#include "DList.h"
+void test1()
 {
 	SListNode* pList = NULL;
 
@@ -20,15 +21,39 @@ int main()
 	SListPopBack(&pList);
 	SListPopBack(&pList);
 	SListPrint(pList);
-	SListPushFront(&pList,0);
+	SListPushFront(&pList, 0);
 	SListPushFront(&pList, 1);
 	SListPushFront(&pList, 2);
 	SListPrint(pList);
 	SListPopFront(&pList);
-	SListPrint(pList); 
-	SListNode* pos= SListFind(pList, 1);
-	pos->date =3;
 	SListPrint(pList);
+	SListNode* pos = SListFind(pList, 1);
+	pos->date = 3;
+	SListPrint(pList);
+}
+	
+void test2()
+{
+	ListNode* phead = ListInit(0);
+	ListPushBack(phead,1);
+	ListPushBack(phead,2);
+	ListPushBack(phead,3);
+	ListPushBack(phead,4);	
+	ListPushFront(phead, 0);
+	ListPushFront(phead, -1);
+	ListPushFront(phead, -2);
+	ListPushFront(phead, -3);
+	ListPopBack(phead);
+	ListPopFront(phead);
+	
 
+	ListPrint(phead);
+	
+	return;
+}
+int main()
+{
+	//test1();
+	test2();
 	return 0;
 }
